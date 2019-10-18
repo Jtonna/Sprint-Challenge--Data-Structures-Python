bd0d0dcba6368685e426de0fad4be0a7cd278137
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-18 11:08:47 -0700

    Moving on temporarily

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -29,25 +29,22 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    remove_current = self.storage.pop(self.current)
-    insert_in_current_with_item = self.storage.insert(self.current, item)
-
-    #  If current is none do thing
-    if self.storage[self.current] == None:
-      # Remove from last peice of entered data (end of list), add new data at the current index, current + 1
-      remove_current()
-      insert_in_current_with_item()
-      self.current += 1
-    #  If current is the capacity 
-    elif self.current == self.capacity:
+    #  If current is the capacity
+    if self.current == self.capacity:
       #  Remove 0 index just incase, insert the item into the first index, set current to 1
       self.storage.pop(0)
       self.storage.insert(0, item)
       self.current = 1
+    #  if the storages list is None
+    elif self.storage[self.current] == None:
+      # Remove from last peice of entered data (end of list), add new data at the current index, current + 1
+      self.storage.pop(self.current)
+      self.storage.insert(self.current, item)
+      self.current += 1
     else:
       #  If none of those conditions are true, we remove current, insert current and increment
-      remove_current()
-      insert_in_current_with_item()
+      self.storage.pop(self.current)
+      self.storage.insert(self.current, item)
       self.current += 1
 
   def get(self):
