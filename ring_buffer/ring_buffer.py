450895bc25aa5919ca83895ade9ea5d69f85ede4
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-18 10:34:50 -0700

    Update ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,12 +2,24 @@
 #  Ring buffers are a FIFO -> First in First Out system.
 #  The "Ring" has a fixed size once that limit is reached it gets deleted (but were not doing that in this project)
 
-#  Spec ---
+#  --- Spec ---
 #  Implement a system to add data (append) to the ring buffer
 #  Implement a system to get ALL of the data from the buffers's list
 
-#  Pre-Mature plan ---
-#  
+#  --- Pre-Mature Plan ---
+#  The list's length starts at None * capacity
+#
+#  So we can check to see if current equals Null
+#     We remove the last peice of data
+#     Insert the new peice of data
+#     Set the current value to 1 for tracking of current as its the first entry
+#  Else if current equals the capacity
+#     Remove the first item in the ring (list)
+#     Increment the counter to counter + 1
+#  Else
+#     If neither of those cases work we need to remove current
+#     Insert the the "item"
+#     Increment the counter by 1
 
 
 class RingBuffer:
