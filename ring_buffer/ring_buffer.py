c52cd7b7e980d6b1c574b3b62e0674e8a0b806c3
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-18 10:11:23 -0700

    Working on a plan

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,6 +1,14 @@
-# Plan ---
-# Ring buffers are a FIFO -> First in First Out system.
-# 
+#  Info ---
+#  Ring buffers are a FIFO -> First in First Out system.
+#  The "Ring" has a fixed size once that limit is reached it gets deleted (but were not doing that in this project)
+
+#  Spec ---
+#  Implement a system to add data (append) to the ring buffer
+#  Implement a system to get ALL of the data from the buffers's list
+
+#  Pre-Mature plan ---
+#  
+
 
 class RingBuffer:
   def __init__(self, capacity):
