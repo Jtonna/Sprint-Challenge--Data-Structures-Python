5a2c57a50fb98e5cbda543309d90a51056d223b9
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-18 12:07:35 -0700

    Its kinda faster

--- names/names.py
+++ names/names.py
@@ -1,7 +1,8 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 #  --- Plan ---
-#  Binary search tree
+#  
 
 start_time = time.time()
 
@@ -14,10 +15,21 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# Call the tree and pass it the names_1[0]
+# for loop that inserts the name into the tree
+# for loops that checks if the tree contains a name
+#   append the name to duplocates because its well a duplicate
+
+bns = BinarySearchTree(names_1[0])
+
+for name_from_list in names_1[1:]:
+    bns.insert(name_from_list)
+
+for name_from_list in names_2:
+    if bns.contains(name_from_list):
+        duplicates.append(name_from_list)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
